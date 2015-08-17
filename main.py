28fee1f4bc77c4d1ea25c8547b3a3336fbbe09ae
sogeking90 <neilharper300890@gmail.com>
2015-08-17 17:51:30 +0100

    Added statue room to the game. Not currently playable

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # It is going to be a fairly basic text adventure with standard commands such as go north, look and i for inventory. 
 # The pet companion can be interacted with also.
 from sys import exit
+import random
 
 # A list which contains the players inventory.
 invent = ["Dog toy", "Pick Axe"]
@@ -23,6 +24,36 @@ def game_help():
 def cls():
     print "\n" * 100
 
+def dog_events():
+    # Random chance of the dog Mia interacting with you.
+    chance = random.randint(1, 61)
+    if chance == 1:
+        print "Mia starts doing somersaults whilst screaming \"Gobshite!\". Huh. Strange."
+
+    elif chance > 1 and chance < 5:
+        print "Mia rolls on her back and looks at you expectantly.\nYou of course rub her belly."
+
+    elif chance > 20 and chance < 10:
+        print "Mia paws at you annoyingly."
+
+def start():
+    print """
+    The young scholar enters the newly opened "Temple of the maker" which has been the subject
+    of mystery for hundreds of years. All that anyone knows for sure is that it was built by 
+    the Elves thousands of years ago. Sadly Elves have been extinct for over a millennia so 
+    it was never as simple as asking an elf how to open the temple. 
+
+    The passage is opened to you not by chance. You spent nearly half your life obsessed with
+    the idea of opening the temple and discovering what mysteries await.
+    After getting your hands on an ancient elvish tome you finally opened the door with the 
+    runes you discovered in the tome.
+
+    -----------------------------------
+    Type 'help' at any time to see a full list of commands. Press Enter to start the game.
+    """
+    raw_input("> ")
+    cls()
+
 def entrance_room():
     playing = True
     description = """
@@ -34,6 +65,7 @@ def entrance_room():
     """
     print description
     while playing:
+        dog_events()
         prompt = raw_input("> ")
 
         if "help" in prompt.lower():
@@ -49,7 +81,8 @@ def entrance_room():
             print "large_room()"
 
         elif prompt.lower() == "go west":
-            print "statue_room()"
+            statue_room()
+            playing = False
         
         elif prompt.lower() == "go east":
             print "fountain_room()"
@@ -60,10 +93,42 @@ def entrance_room():
         else:
             print "I don't understand what you mean."
 
+def statue_room():
+    playing = True
+    description = """
+    This room appears to be some sort of a shrine to the Maker. The statue itself take up half of 
+    the room. The Maker looks down at you like an unwelcome guest. It just occurred to me that it
+    is maybe unusual that the torches are all still lit in this building after thousands of years.
+    The elves are long gone but their magic is still strong it seems. 
+
+    Just at the statue's feet you see a basket with some kind of token. A tribute to the Maker perhaps?
+
+    There is nowhere to go but back into the entrance chamber to the east. 
+    """
+    print description
+    while playing:
+        dog_events()
+        prompt = raw_input("> ")
+
+        if "help" in prompt.lower():
+            game_help()
     
+        elif "cls" in prompt.lower():
+            cls()
+
+        elif "look" in prompt.lower():
+            print description
+
+        elif prompt.lower() == "go east":
+            entrance_room()
+            playing = False
+        
+        elif "take token" in prompt or "take tribute" in prompt:
+            print "You pick up the token and place it in your backpack."
+            invent.append("token")
 
 
-
+start()
 entrance_room()
 
 
